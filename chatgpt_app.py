aa3c2af7fc0426b462da1ba529805b18c80caf9e
tirohan <tanbinislam009@gmail.com>
2023-01-23 14:32:34 +0600

    code added

---
+++ chatgpt_app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import openai 
+
+st.title("Let's Chat")
+st.sidebar.header("Instructions")
+st.sidebar.info(
+    '''This is a web application that allows you to interact with 
+       the OpenAI API's implementation of the ChatGPT model.
+       Enter a **query** in the **text box** and **press enter** to receive 
+       a **response** from the ChatGPT
+       '''
+)
+
+model_engine = "text-davinci-003"
+openai.api_key = "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"
+
+
+def main():
+    # Get user input
+    user_query = st.text_input(
+        "Enter query here, to exit enter :q", "what is Machine Learning?")
+    if user_query != ":q" or user_query != "":
+        # Pass the query to the ChatGPT subprocess
+        response = ChatGPT(user_query)
+        return st.write(f"{user_query} {response}")
+
+# Use the OpenAI API to generate a response
+
+def ChatGPT(user_query):
+
+
+    completion = openai.Completion.create(
+        engine=model_engine,
+        prompt=user_query,
+        max_tokens=1024,
+        n=1,
+        temperature=0.5,
+    )
+    response = completion.choices[0].text
+    return response
+
+
+main()
+
